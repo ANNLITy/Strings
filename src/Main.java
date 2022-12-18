e14c9d3825fda647e599df1c8bacafbbdef6057f
Al040 <klpk44@yandex.ru>
2022-12-18 12:20:25 +0300

    dz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
+//        Задача 1
+        String firstName = " Ivan ";
+        String middleName = " Ivanovich ";
+        String lastName = " Ivanov ";
+        String fullName = lastName+ firstName+ middleName;
+        System.out.println("ФИО сотрудника - "+fullName);
+        // Задача 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —  "+fullName.toUpperCase());
+        // Задача 3
+        String FullName = " Иванов Семён Семёнович";
+        FullName = FullName.replace("ё","е");
+        System.out.println("Данные ФИО сотрудника —  "+FullName);
 
 
     }
